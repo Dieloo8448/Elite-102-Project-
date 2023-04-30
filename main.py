5a3209895437b7eea14dbc4cfa36ce37961d976a
Dieloo8448 <97068208+Dieloo8448@users.noreply.github.com>
2023-04-30 17:28:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import mysql.connector
 
 # Connect to the database
-conn = mysql.connector.connect(
+db = mysql.connector.connect(
     host="localhost",
     first_name="first name",
     last_name="last name",
@@ -11,10 +11,15 @@ conn = mysql.connector.connect(
     address="address",
     username="username",
     password="password",
-    database="bank"
+    balance="balance",
+    database="RBCFU Bank"
 )
-c = conn.cursor()
+c = db.cursor()
 
+c.execute("SHOW TABLES")
+ 
+for x in c:
+  print(x)
 
 def create_user():
     # Get the values from the input fields
@@ -25,11 +30,12 @@ def create_user():
     address = entry_address.get()
     username = entry_username.get()
     password = entry_password.get()
+    balance = entry_balance.get()
     
     # Insert the values into the database
-    c.execute("INSERT INTO users (first_name, last_name, dob, phone, address, username, password) VALUES (?, ?, ?, ?, ?, ?, ?)",
-              (first_name, last_name, dob, phone, address, username, password))
-    conn.commit()
+    c.execute("INSERT INTO users (first_name, last_name, dob, phone, address, username, password,balance) VALUES (?, ?, ?, ?, ?, ?, ?)",
+              (first_name, last_name, dob, phone, address, username, password,balance))
+    db.commit()
 
     # Clear the input fields
     entry_first_name.delete(0, tk.END)
@@ -39,73 +45,81 @@ def create_user():
     entry_address.delete(0, tk.END)
     entry_username.delete(0, tk.END)
     entry_password.delete(0, tk.END)
+    entry_balance.delete(0,tk.END)
 
 # Create the UI
 root = tk.Tk()
+root['background']='#FFFFFF'
 root.title("Create User")
 
 # Frames
-frame_personal = tk.LabelFrame(root, text="Personal Information", padx=12, pady=12)
+frame_personal = tk.LabelFrame(root, text="Personal Information", padx=12, pady=12,fg='black', bg='white')
 frame_personal.grid(row=0, column=0, padx=12, pady=12)
 frame_login = tk.LabelFrame(root, text="Login Information", padx=12, pady=12)
 frame_login.grid(row=0, column=1, padx=12, pady=12)
 
 # First Name
-label_first_name = tk.Label(frame_personal, text="First Name")
+label_first_name = tk.Label(frame_personal, text="First Name",bg='white')
 label_first_name.grid(row=0, column=0, sticky=tk.W)
 entry_first_name = tk.Entry(frame_personal)
 entry_first_name.grid(row=0, column=1)
 
 # Last Name
-label_last_name = tk.Label(frame_personal, text="Last Name")
+label_last_name = tk.Label(frame_personal, text="Last Name",bg='white')
 label_last_name.grid(row=1, column=0, sticky=tk.W)
 entry_last_name = tk.Entry(frame_personal)
 entry_last_name.grid(row=1, column=1)
 
 # Date of Birth
-label_dob = tk.Label(frame_personal, text="Date of Birth (YYYY-MM-DD)")
+label_dob = tk.Label(frame_personal, text="Date of Birth (YYYY-MM-DD)",bg='white')
 label_dob.grid(row=2, column=0, sticky=tk.W)
 entry_dob = tk.Entry(frame_personal)
 entry_dob.grid(row=2, column=1)
 
 # Phone Number
-label_phone = tk.Label(frame_personal, text="Phone Number")
+label_phone = tk.Label(frame_personal, text="Phone Number",bg='white')
 label_phone.grid(row=3, column=0, sticky=tk.W)
 entry_phone = tk.Entry(frame_personal)
 entry_phone.grid(row=3, column=1)
 
 # Address
-label_address = tk.Label(frame_personal, text="Address")
+label_address = tk.Label(frame_personal, text="Address",bg='white')
 label_address.grid(row=4, column=0, sticky=tk.W)
 entry_address = tk.Entry(frame_personal)
 entry_address.grid(row=4, column=1)
 
 # Username
-label_username = tk.Label(frame_personal, text="Username")
+label_username = tk.Label(frame_personal, text="Username",bg='white')
 label_username.grid(row=5, column=0, sticky=tk.W)
 entry_username = tk.Entry(frame_personal)
 entry_username.grid(row=5, column=1)
 
 # Password
-label_password = tk.Label(frame_personal, text="Password")
+label_password = tk.Label(frame_personal, text="Password",bg='white')
 label_password.grid(row=6, column=0, sticky=tk.W)
 entry_password = tk.Entry(frame_personal)
 entry_password.grid(row=6, column=1)
 
+# Balance
+label_balance = tk.Label(frame_personal, text="Balance",bg='white')
+label_balance.grid(row=7, column=0, sticky=tk.W)
+entry_balance = tk.Entry(frame_personal)
+entry_balance.grid(row=7, column=1)
+
 # Create User button
-button_create = tk.Button(root, text="Create User", command=create_user)
+button_create = tk.Button(root, text="Create User", command=create_user,fg='black', bg='lightgreen')
 button_create.grid(row=7, column=0)
 
 def close_account():
-    account_number = input("Enter the account number to close: ")
+    username = input("Enter the account number to close: ")
 
     # Delete the account and associated records from the database
-    c.execute("DELETE FROM users WHERE account_number = %s", (account_number,))
-    c.execute("DELETE FROM accounts WHERE account_number = %s", (account_number,))
-    c.execute("DELETE FROM transactions WHERE account_number = %s", (account_number,))
-    conn.commit()
+    c.execute("DELETE FROM users WHERE username = %s", (username,))
+    c.execute("DELETE FROM accounts WHERE username = %s", (username,))
+    c.execute("DELETE FROM transactions WHERE username = %s", (username,))
+    c.commit()
 
-    print("Account " + account_number + " has been closed")
+    print("Account " + username + " has been closed")
 
 
 def login():
@@ -123,11 +137,16 @@ def login():
         print("Welcome, " + user[1])
         return user[0]
 
-
-    
 def check_account_balance(id: int) -> int:
-    sql = f"""SELECT balance FROM `{username}`.`user` where id = {id};"""
-    cur = conn.cursor()
-    cur.execute(sql)
-    result = cur.fetchone()
-    return result[0]
+    username = input("Enter your username: ")
+    password = input("Enter your Password: ")
+    sql = "SELECT balance FROM accounts WHERE account_number = %s AND pin = %s"
+    val = (username, password)
+    c.execute(sql, val)
+    result = c.fetchone()
+    
+    if result:
+      balance = result[0]
+      print("Your account balance is:", balance)
+    else:
+      print("Invalid username or password.")
